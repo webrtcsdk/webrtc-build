13d248e1f804fd4179fda5047637eefe78355d24
lambiengcode <lambiengcode@gmail.com>
2023-07-26 21:43:09 +0700

    update .

--- build/run.py
+++ build/run.py
@@ -866,10 +866,10 @@ def package_webrtc(source_dir, build_dir, package_dir, target,
     ts = []
     for t in get_build_targets(target):
         ts += ['--target', t]
-    cmd(['python3', os.path.join(webrtc_src_dir, 'tools_webrtc', 'libs', 'generate_licenses.py'),
-        *ts, webrtc_package_dir, *dirs])
-    os.rename(os.path.join(webrtc_package_dir, 'LICENSE.md'),
-              os.path.join(webrtc_package_dir, 'NOTICE'))
+    # cmd(['python3', os.path.join(webrtc_src_dir, 'tools_webrtc', 'libs', 'generate_licenses.py'),
+    #     *ts, webrtc_package_dir, *dirs])
+    # os.rename(os.path.join(webrtc_package_dir, 'LICENSE.md'),
+    #           os.path.join(webrtc_package_dir, 'NOTICE'))
 
     # ヘッダーファイルをコピー
     # Copy headers
